420552f0e58612338178551714bbde0b1bfde3ec
kkazlov <kkazlov.it@gmail.com>
2021-10-29 13:04:43 +0300

    s1 - dynamic loading  problems

--- server/controllers/member-controller.js
+++ server/controllers/member-controller.js
@@ -1,55 +1,60 @@
 import memberService from "../services/member-service.js";
 
-
 class memberController {
-    async create(req, res) {
-        try {
-            const member = await memberService.create(req.body);
-            res.json(member);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    }
+	async create(req, res) {
+		try {
+			const member = await memberService.create(req.body);
+			res.json(member);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
 
-    async getAll(req, res) {
-        try {
-            const members = await memberService.getAll();
-            return res.json(members);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    }
+	async getAll(req, res) {
+		try {
+            const {pos = 0} = req.query;
+			
+			const members = await memberService.getAll();
+            const webixObj = {
+                data: [...members],
+                pos,
+                total_count: members.length
+            }
+			return res.json(webixObj);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
 
-    async getOne(req, res) {
-        try {
-            const {id} = req.params;
-            const member = await memberService.getOne(id)
-            return res.json(member);
+	async getOne(req, res) {
+		try {
+			const {id} = req.params;
+			const member = await memberService.getOne(id);
+			return res.json(member);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
 
-        } catch (error) {
-            res.status(500).json(error);
-        } 
-    }
+	async update(req, res) {
+		try {
+			const member = req.body;
+			const updatedMember = await memberService.update(member);
+			return res.json(updatedMember);
+		} catch (error) {
+			res.status(500).json(error.message);
+		}
+	}
 
-    async update(req, res) {
-        try {
-            const member = req.body;
-            const updatedMember = await memberService.update(member);
-            return res.json(updatedMember);
-        } catch (error) {
-            res.status(500).json(error.message);
-        }
-    }
-
-    async delete(req, res) {
-        try {
-            const {id} = req.params;
-            const member = await memberService.delete(id);
-            return res.json(member);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    }
+	async delete(req, res) {
+		try {
+			const {id} = req.params;
+			const member = await memberService.delete(id);
+			return res.json(member);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
 }
 
 export default new memberController();
--- sources/views/members/index.js
+++ sources/views/members/index.js
@@ -1,9 +1,26 @@
 import {JetView} from "webix-jet";
 
+import membersDB from "../../models/membersDB";
+
 export default class Members extends JetView {
 	config() {
 		return {
-			template: "members"
+			view: "datatable",
+			columns: [
+				{id: "Name", header: "Name", fillspace: 1},
+				{id: "Role", header: "Role", fillspace: 1},
+				{id: "BirthDate", header: "Birth Date", fillspace: 1},
+				{id: "Country", header: "Country", fillspace: 1},
+				{id: "Awards", header: "Awards", fillspace: 1}
+			],
+			datathrottle: 500,
+			datafetch: 20
+			/* url: "http://localhost:5000/api/members" */
 		};
+	}
+
+	init(view) {
+		const url = "http://localhost:5000/api/members";
+		view.load(url);
 	}
 }
