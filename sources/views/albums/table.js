bdcceafb462e5380d771c28650a48c4d444d8a0e
kkazlov <kkazlov.it@gmail.com>
2021-11-26 09:21:38 +0300

    s2 - albums. fixed showOverlay

--- sources/views/albums/table.js
+++ sources/views/albums/table.js
@@ -24,6 +24,7 @@ export default class Table extends AlbumsTableConstr {
 				this.groupName = Name;
 
 				if (this.GroupID) {
+					this.table.showOverlay("Loading...");
 					this.loadAlbums().then((albums) => {
 						this.table.hideOverlay();
 						this.table.parse(albums);
